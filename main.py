ff02e1e3a091b9d29445b03e943084f0d3f355a2
stonkchonk <heilfred98@gmail.com>
2025-01-06 00:03:13 +0100

    latex code generation

--- main.py
+++ main.py
@@ -110,29 +110,107 @@ def intersect_calculator(intrinsic_matrix, bb_tensor: Tensor) -> float:
     return math.sqrt(world_position[0]**2 + world_position[2]**2)
 
 
+# latex printers
+def print_figure_description(name, precision_val, recall_val, num_ground_truths, num_detections):
+    print("\\begin{figure}[H]")
+    print("\t\\centering")
+    print("\t\\includegraphics[width=1\\columnwidth]{Figures/o_" + name + ".png}")
+    print("\t\\caption{" + name + f".png, Ground Truths: {num_ground_truths}, Detections: {num_detections}, Precision: {str(round(precision_val, 2))}, Recall: {str(round(recall_val, 2))}" +"}")
+    print("\t\\label{o_" + name + "}")
+    print("\\end{figure}")
+
+
+def print_iou_matrix(gt_bbs_tensor, detected_bbs_tensor):
+    if gt_bbs_tensor.size(dim=0) == 0 or detected_bbs_tensor.size(dim=0) == 0:
+        print("Cannot provide IoU $n$ by $m$ matrix if $n$ or $m$ is zero.")
+    else:
+        iou = box_iou(gt_bbs_tensor, detected_bbs_tensor)
+        n_ground_truths, m_detections = iou.shape[0], iou.shape[1]
+
+        print("\\[")
+        print("IoU=")
+        print("\\begin{bmatrix}")
+        row_strings = []
+        for i in range(0, n_ground_truths):
+            row_elements = []
+            for j in range(0, m_detections):
+                row_elements.append(str(round(iou[i, j].item(), 2)))
+            row_string = " & ".join(row_elements)
+            row_strings.append(row_string)
+        matrix_string = " \\\\\n".join(row_strings)
+        print(matrix_string)
+        print("\\end{bmatrix}")
+        print("\\]")
+
+
+def print_assigned_unassigned(matching_result_object: tuple[dict, list] | None):
+    if matching_result_object is None:
+        print("Could not match any ground truths with detections.")
+    else:
+        print("Assignments (ground truth \\(\\rightarrow\\) detection (IoU)):\\newline")
+        matching_strings = []
+        for idx in matching_result[0].keys():
+            match = matching_result[0].get(idx)
+            if match is None:
+                matching_strings.append(str(idx) + "\\(\\rightarrow\\)None")
+            else:
+                detection_idx = match[0]
+                corresponding_iou = match[1]
+                matching_strings.append(str(idx) + "\\(\\rightarrow\\)" + str(detection_idx) + f" (IoU={str(round(corresponding_iou, 2))})")
+        assignments_string = ", ".join(matching_strings)
+        print("[" + assignments_string + "]\\newline\\newline")
+        print("Unassigned detections:\\newline")
+        unassigned_string = ", ".join(list(map(lambda x: str(x), matching_result_object[1])))
+        print("[" + unassigned_string + "]\\newline\\newline")
+
+
+def print_detection_distances(matching_result_object: tuple[dict, list] | None, object_labels: list[ObjectLabel]):
+    if matching_result_object is None:
+        pass
+    else:
+        print("Calculated vs. ground truth distance for matched detections:\\newline")
+        for idx in matching_result[0].keys():
+            match = matching_result[0].get(idx)
+            if match is not None:
+                bb_tensor = match[2]
+                detection_idx = match[0]
+                distance = intersect_calculator(labeled_image.intrinsic_matrix, bb_tensor)
+                print(f"{detection_idx}\\(\\rightarrow\\) calculated: {str(round(distance, 2))}m, ground truth: {str(round(object_labels[idx].distance, 2))}m\\newline")
+
+
+
+
 data_set = DataSet.load_from_directory('KITTI_Selection')
 model = YOLO("yolo11n.pt")
 
+# generate overview plot
 plot_calculated_distances = []
 plot_ground_truth_distances = []
-
 for labeled_image in data_set.labeled_images:
-    predicted_result = (model.predict(labeled_image.image_data, classes=[2]))[0]  # filter for cars (class 2)
+    predicted_result = (model.predict(labeled_image.image_data, classes=[2], verbose=False))[0]  # filter for cars (class 2)
     matching_result = iou_bb_matcher(labeled_image.object_labels_as_tensor, predicted_result.boxes.xyxy.data)
-    print(labeled_image.name)
-    print(matching_result)
     if matching_result is not None:
         for idx in matching_result[0].keys():
             match = matching_result[0].get(idx)
             if match is not None:
                 bb_tensor = match[2]
                 distance = intersect_calculator(labeled_image.intrinsic_matrix, bb_tensor)
-                print(f'gt {idx}, calculated {distance}, actual {labeled_image.object_labels[idx].distance}')
                 plot_calculated_distances.append(distance)
                 plot_ground_truth_distances.append(labeled_image.object_labels[idx].distance)
 
+    # print latex results
     precision, recall = precision_and_recall(matching_result)
-    print(f'precision {precision}, recall {recall}')
+    print_figure_description(labeled_image.name, precision, recall, len(labeled_image.object_labels), predicted_result.boxes.xyxy.data.size(dim=0))
+    #print("\\begin{tabular}{p{0.45\\textwidth} p{0.45\\textwidth}}")
+    #print_iou_matrix(labeled_image.object_labels_as_tensor, predicted_result.boxes.xyxy.data)
+    #print("&")
+    print_assigned_unassigned(matching_result)
+    #print("\\\\")
+    #print("\\end{tabular}\\newline")
+    print_detection_distances(matching_result, labeled_image.object_labels)
+    print("\n")
+
+    # generate output image
     cv2.imwrite("output/o_" + labeled_image.name + ".png", bb_drawer(labeled_image.object_labels,
                                                                      predicted_result.boxes.xyxy.data,
                                                                      labeled_image.image_data))
